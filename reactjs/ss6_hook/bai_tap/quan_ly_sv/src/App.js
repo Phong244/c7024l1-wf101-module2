d00d2204cc732fc56a7ff2c5d1fff1d6bb46a193
phong244 <phong.levanthanh@gmail.com>
2024-11-21 11:05:51 +0700

    update ngay

---
+++ reactjs/ss6_hook/bai_tap/quan_ly_sv/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import React from "react";
+import ListStudent from "./components/ListStudent";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
+
+function App() {
+  return (
+    <>
+      <ListStudent />
+    </>
+  );
+}
+
+export default App;
