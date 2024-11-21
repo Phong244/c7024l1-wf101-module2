42f011963bf23f0e418f8e008c80a86350fb111f
phong244 <phong.levanthanh@gmail.com>
2024-11-21 20:48:30 +0700

    update ngay

---
+++ reactjs/ss6_hook/bai_tap/quan_ly_sv/src/components/DeleteStudent.js
@@ -0,0 +1,38 @@
+import React from "react";
+import {deleteStudentById} from "../service/studentService";
+
+function DeleteStudent({isShowModal, deleteStudent, handleIsLoading, handleCloseModal}) {
+    const handleDelete = () => {
+        deleteStudentById(deleteStudent.id);
+        handleIsLoading();
+        handleCloseModal();
+    }
+    return (
+        <>
+            {isShowModal && (
+                <div className="modal show" tabIndex="-1" style={{display: 'block'}}>
+                    <div className="modal-dialog">
+                        <div className="modal-content">
+                            <div className="modal-header">
+                                <h5 className="modal-title">Delete</h5>
+                                <button type="button" className="btn-close" data-bs-dismiss="modal"
+                                        aria-label="Close"></button>
+                            </div>
+                            <div className="modal-body">
+                                <p>Do you want to delete {deleteStudent.name}???</p>
+                            </div>
+                            <div className="modal-footer">
+                                <button onClick={handleCloseModal} type="button" className="btn btn-secondary"
+                                        data-bs-dismiss="modal">Close
+                                </button>
+                                <button onClick={handleDelete} type="button" className="btn btn-primary">Delete</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            )}
+        </>
+    );
+}
+
+export default DeleteStudent;
--- reactjs/ss6_hook/bai_tap/quan_ly_sv/src/components/ListStudent.js
+++ reactjs/ss6_hook/bai_tap/quan_ly_sv/src/components/ListStudent.js
@@ -1,11 +1,15 @@
-import React, {useEffect, useState} from "react";
-import {getAllStudent} from "../service/studentService";
+import React, {useEffect, useRef, useState} from "react";
+import {getAllStudent, searchByName} from "../service/studentService";
 import AddStudent from "./AddStudent";
-
+import DeleteStudent from "./DeleteStudent";
 
 const ListStudent = () => {
     const [studentList, setStudentList] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
+    const searchNameRef = useRef();
+    const [isShowModal, setIsShowModal] = useState(false);
+    const [deleteStudent, setDeleteStudent] = useState({});
+
 
     useEffect(() => {
         setStudentList(() => (
@@ -19,9 +23,33 @@ const ListStudent = () => {
         setIsLoading((prevState) => !prevState)
     }
 
+    const handleSearch = () => {
+        const searchName = searchNameRef.current.value;
+        const listSearch = searchByName(searchName);
+        setStudentList(() => (
+            [
+                ...listSearch
+            ]
+        ));
+    }
+
+    const handleShowModal = (student) => {
+        setDeleteStudent(() => ({
+            ...student
+        }))
+        setIsShowModal(prevState => !prevState);
+    }
+    const handleCloseModal = () => {
+        setIsShowModal(prevState => !prevState);
+    }
+
     return (
         <>
             <AddStudent handleIsLoading={handleIsLoading}/>
+            <form>
+                <input ref={searchNameRef} placeholder={'Enter name'}/>
+                <button type={'button'} onClick={handleSearch}>Search</button>
+            </form>
             <table className={'table table-dark'}>
                 <thead>
                 <tr>
@@ -32,18 +60,23 @@ const ListStudent = () => {
                 </tr>
                 </thead>
                 <tbody>
-                {studentList.map((customer, i) => (
-                    <tr key={customer.id}>
+                {studentList.map((student, i) => (
+                    <tr key={student.id}>
                         <td>{i + 1}</td>
-                        <td>{customer.id}</td>
-                        <td>{customer.name}</td>
+                        <td>{student.id}</td>
+                        <td>{student.name}</td>
                         <td>
-                            <button className={'btn btn-sm btn-danger'}>Delete</button>
+                            <button onClick={() => {
+                                handleShowModal(student);
+                            }} className={'btn btn-sm btn-danger'}>Delete
+                            </button>
                         </td>
                     </tr>
                 ))}
                 </tbody>
             </table>
+            <DeleteStudent handleIsLoading={handleIsLoading} isShowModal={isShowModal} deleteStudent={deleteStudent}
+                           handleCloseModal={handleCloseModal}/>
         </>
     );
 }
